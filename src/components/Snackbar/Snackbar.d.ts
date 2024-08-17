73c4d53323a0b90aefa1cff992bfcc76e4bbc8c6
Navinoxx <lorenzo0100@hotmail.com>
2024-08-17 19:52:20 -0300

    ad types

--- src/components/Snackbar/Snackbar.d.ts
+++ src/components/Snackbar/Snackbar.d.ts
@@ -1,6 +1,13 @@
 import { FC } from "react";
 
 interface SnackbarProps {
+    autoHideDuration?: number
+    disableWindowBlurListener?: boolean
+    onClose?: () => void
+    open?: boolean
+    resumeHideDuration?: number
+    message?: string
+    position?: "topLeft" | "topCenter" | "topRight" | "bottomLeft" | "bottomCenter" | "bottomRight"
     children: React.ReactNode
     className?: string
 }
